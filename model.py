dae9368e3550a6f18041dc2c17a1f34c9f673111
takam5f2 <taka.akamine@gmail.com>
2018-09-06 22:22:26 +0900

    Better result.

---
+++ model.py
@@ -0,0 +1,77 @@
+# model.py
+# this source code includes model for control vehicle steering wheel angle
+# with machine learning system.
+# copyright: Takayuki AKAMINE
+
+# model 
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Lambda, Dropout, Activation
+from keras.layers.convolutional import Conv2D, Cropping2D
+from keras.layers.pooling import MaxPool2D
+from keras.layers.normalization import BatchNormalization
+
+def build_mymodel(model, ch=3, row=160, col=320):
+    
+
+    # Preprocess incoming data, centered around zero with small standard deviation
+    model.add(Lambda(lambda x: (x / 128.0) - 1.0,
+                     input_shape=(row, col, ch),
+                     output_shape=(row, col, ch)))
+    # model.add(Cropping2D(cropping=((60,20), (0,0)))) # including hyperparameter
+    # VGG-like Neural Network
+    # convolutional
+    for i in range(2):
+         model.add(Conv2D(6, (3, 3), strides=(2,2), padding='same'))
+         model.add(BatchNormalization(epsilon=1e-07))
+         model.add(Activation('relu'))
+    # max pooling
+    model.add(MaxPool2D(pool_size=(2,2)))
+
+    # convolutional
+    for i in range(2):
+        model.add(Conv2D(6, (3, 3), strides=(2,2), padding='same'))
+        model.add(BatchNormalization(epsilon=1e-07))
+        model.add(Activation('relu'))
+    # max pooling
+    model.add(MaxPool2D(pool_size=(2,2)))
+
+    # convolutional
+    for i in range(3):
+        model.add(Conv2D(24, (3, 3), strides=(1,1), padding='same'))
+        model.add(BatchNormalization(epsilon=1e-07))
+        model.add(Activation('relu'))
+    # max pooling
+    model.add(MaxPool2D(pool_size=(2,2)))
+
+    # convolutional
+    model.add(Conv2D(48, (3, 3), strides=(1,1), padding='same'))
+    model.add(BatchNormalization(epsilon=1e-07))
+    model.add(Activation('relu'))
+    
+    # flatten
+    model.add(Flatten())
+
+    # fully
+    model.add(Dense(800))
+    model.add(BatchNormalization(epsilon=1e-07))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(600)) 
+    model.add(BatchNormalization(epsilon=1e-07))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(300))
+    model.add(BatchNormalization(epsilon=1e-07))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(120))
+    model.add(BatchNormalization(epsilon=1e-07))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+    
+    return model
--- model_execution.py
+++ model_execution.py
@@ -4,9 +4,12 @@
 
 import os
 import csv
+from keras.utils import plot_model
 
 samples = []
-with open('../sample_data/driving_log.csv') as csvfile:
+data_path = '../sample_data/'
+img_path = data_path + 'IMG/'
+with open(data_path+'driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
@@ -15,13 +18,14 @@ from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 
+# data augment
+from data_augment import data_augment
+train_samples = data_augment(train_samples, img_path=img_path)
+
 from generator import generator
 # compile and train the model using the generator function
-img_path = '../sample_data/IMG/'
 train_generator = generator(train_samples, batch_size=32, img_path=img_path)
 validation_generator = generator(validation_samples, batch_size=32, img_path=img_path)
-
-# data augment
 
 ch, row, col = 3, 160, 320  # Trimmed image format
 
@@ -31,8 +35,10 @@ from model import build_mymodel
 model = Sequential()
 model = build_mymodel(model, ch, row, col)
 
+plot_model(model, to_file='mymodel.png')
+
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch = len(train_samples),
+model.fit_generator(train_generator, steps_per_epoch = len(train_samples)/8,
                     validation_data=validation_generator,
-                      nb_val_samples = len(validation_samples), nb_epoch=25)
+                    validation_steps = len(validation_samples)/8, epochs=5)
 model.save('model.h5')
